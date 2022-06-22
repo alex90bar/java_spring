17ffa03b3fa2f56a820dd286487a0020fff0d2f2
alex90bar <aleksandr.baranov@skillbox.ru>
2022-06-22 17:56:52 +0300

    9.2 Unit-tests for jwtLogin, bookService and book popularity indexes written

--- src/test/java/com/example/MyBookShopApp/security/BookStoreUserRegisterTest.java
+++ src/test/java/com/example/MyBookShopApp/security/BookStoreUserRegisterTest.java
@@ -2,6 +2,9 @@ package com.example.MyBookShopApp.security;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.example.MyBookShopApp.security.jwt.JWTBlacklistRepository;
+import com.example.MyBookShopApp.security.jwt.JWTUtil;
+import javax.naming.AuthenticationException;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +13,10 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
@@ -17,17 +24,26 @@ class BookStoreUserRegisterTest {
 
   private final BookStoreUserRegister bookStoreUserRegister;
   private final PasswordEncoder passwordEncoder;
+  private final JWTUtil jwtUtil;
   private RegistrationForm registrationForm;
+  private ContactConfirmationPayload payload;
 
   @MockBean
   private BookstoreUserRepository bookstoreUserRepositoryMock;
 
+  @MockBean
+  private JWTBlacklistRepository jwtBlacklistRepository;
+
+  @MockBean
+  private AuthenticationManager authenticationManager;
+
   @Autowired
   public BookStoreUserRegisterTest(
       BookStoreUserRegister bookStoreUserRegister,
-      PasswordEncoder passwordEncoder) {
+      PasswordEncoder passwordEncoder, JWTUtil jwtUtil) {
     this.bookStoreUserRegister = bookStoreUserRegister;
     this.passwordEncoder = passwordEncoder;
+    this.jwtUtil = jwtUtil;
   }
 
   @BeforeEach
@@ -37,11 +53,16 @@ class BookStoreUserRegisterTest {
     registrationForm.setName("Tester");
     registrationForm.setPass("qddqd");
     registrationForm.setPhone("79031234567");
+
+    payload = new ContactConfirmationPayload();
+    payload.setCode(registrationForm.getPass());
+    payload.setContact(registrationForm.getEmail());
   }
 
   @AfterEach
   void tearDown() {
     registrationForm = null;
+    payload = null;
   }
 
   @Test
@@ -66,4 +87,21 @@ class BookStoreUserRegisterTest {
     BookstoreUser user = bookStoreUserRegister.registerNewUser(registrationForm);
     assertNull(user);
   }
+
+  @Test
+  void jwtLoginTest(){
+    BookstoreUser user = bookStoreUserRegister.registerNewUser(registrationForm);
+
+    Mockito.doReturn(user)
+        .when(bookstoreUserRepositoryMock)
+        .findBookstoreUserByEmail(user.getEmail());
+
+    ContactConfirmationResponse response = bookStoreUserRegister.jwtLogin(payload);
+
+    BookstoreUserDetails userDetails = new BookstoreUserDetails(user);
+
+    assertTrue(CoreMatchers.is(jwtUtil.validateToken(response.getResult(), userDetails)).matches(true));
+
+  }
+
 }
